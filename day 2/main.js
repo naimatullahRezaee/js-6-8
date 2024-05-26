a2c26016d4855883e0ff513fb45c7f7be3579d4b
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-05-26 07:32:58 +0430

    day 2

---
+++ day 2/main.js
@@ -0,0 +1,42 @@
+// var x = 4;
+// x = 20;
+
+// console.log(x);
+// document.write(x);
+
+// let y = 8;
+// document.write(y);
+
+// const a = "ali";
+// document.write(a);
+
+// b = 40;
+// document.write(b);
+
+// var x;
+// var x = 23;
+// document.write(x);
+
+// let x ;
+// let x =20;
+// const x ;
+// const x =20;
+
+// $name
+// Name
+// first_name
+
+// ali23
+// 34
+
+// _name
+
+// first_name
+
+var name = "ali";
+var NAME = "ali2";
+var Name = "ali3";
+
+document.write(NAME);
+// document.write(NAME);
+// document.write(Name);
