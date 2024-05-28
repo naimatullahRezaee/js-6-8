c11760908a52ef984ebe0b0ef435121e7a5ff597
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-05-28 06:39:46 +0430

    day 4

---
+++ day 4/main.js
@@ -0,0 +1,51 @@
+// String data type
+
+// let name = "ali";
+// let lastname = "karimi";
+
+// let x = "12";
+
+// console.log(name);
+// console.log(typeof name);
+
+// console.log(x);
+// console.log(typeof x);
+
+// console.log(lastname);
+
+// let y = Hi;
+
+// console.log(y);
+
+// let name = "ali";
+// let lastname = "karimi";
+
+// let x = 100;
+
+// console.log(name + " " + lastname + x);
+
+// let x = 20;
+// let y = 10;
+
+// console.log(name + x + y);
+
+// number and bigint datatypes
+
+// var x = BigInt("231223456789234567895678567890456789");
+// console.log(x);
+// console.log(typeof x);
+
+// Boolean datatype
+
+// var x = true;
+// var y = false;
+
+// console.log(x);
+// console.log(typeof x);
+
+// console.log(y);
+// console.log(typeof y);
+
+let x = 2 < 0;
+console.log(x);
+console.log(typeof x);
