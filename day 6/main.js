9e47086c8df11348240b7377b9c9bbd188683430
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-05-30 07:28:51 +0430

    day 6

---
+++ day 6/main.js
@@ -0,0 +1,25 @@
+// let x = 10;
+
+// if (x == 0) {
+//   console.log("this number is Zero");
+// } else if (x % 2 == 0) {
+//   console.log("this number is Even");
+// } else {
+//   console.log("this number is Odd");
+// }
+
+// if (x == 0) {
+//   console.log("this number is Zero");
+// } else if (x > 0) {
+//   console.log("this number is Positive");
+// } else {
+//   console.log("this number is Negative");
+// }
+
+// var age = 3;
+
+// if (age >= 18 && age <= 65) {
+//   console.log("you can work in government");
+// } else {
+//   console.log("you can't work in government");
+// }
---
+++ day 6/script.js
@@ -0,0 +1,34 @@
+let day = 0;
+
+switch (day) {
+  case 0:
+    console.log("Saturday");
+
+    break;
+  case 1:
+    console.log("Sunday");
+    break;
+
+  case 2:
+    console.log("Monday");
+    break;
+
+  case 3:
+    console.log("Tuesday");
+    break;
+
+  case 4:
+    console.log("Wednesday");
+    break;
+
+  case 5:
+    console.log("Thursday");
+    break;
+
+  case 6:
+    console.log("Friday");
+    break;
+
+  default:
+    console.log("invalid day");
+}
