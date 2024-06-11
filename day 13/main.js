73ae133346584e12ef216d023e2644629e5bf5f5
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-06-11 07:35:43 +0430

    example

--- day 13/main.js
+++ day 13/main.js
@@ -1,13 +1,59 @@
-function factorial(n) {
-  if (n < 1) {
-    return 1;
-  } else {
-    // n *n -1
+// const salary = [2000, 30000, 10000, 400000, 200000, 230000, 1000, 3000];
 
-    return n * factorial(n - 1);
-  }
+// for (s of salary) {
+//   if (s < 5000) {
+//     console.log(s);
+//   } else if (s > 5000 && s <= 15000) {
+//     tax = s * (5 / 100);
+//     s = s - tax;
+//     console.log(s);
+//   } else if (s > 15000 && s <= 30000) {
+//     tax = s * (10 / 100);
+//     s = s - tax;
+//     console.log(s);
+//   } else if (s > 30000 && s <= 50000) {
+//     tax = s * (12 / 100);
+//     s = s - tax;
+//     console.log(s);
+//   } else {
+//     tax = s * (15 / 100);
+//     s = s - tax;
+//     console.log(s);
+//   }
+// }
+
+s = parseInt(prompt("Enter your salary:  "));
+
+if (s < 5000) {
+  console.log(s);
+} else if (s > 5000 && s <= 15000) {
+  tax = s * (5 / 100);
+  s = s - tax;
+  console.log(s);
+} else if (s > 15000 && s <= 30000) {
+  tax = s * (10 / 100);
+  s = s - tax;
+  console.log(s);
+} else if (s > 30000 && s <= 50000) {
+  tax = s * (12 / 100);
+  s = s - tax;
+  console.log(s);
+} else {
+  tax = s * (15 / 100);
+  s = s - tax;
+  console.log(s);
 }
-console.log(factorial(5));
+
+// function factorial(n) {
+//   if (n < 1) {
+//     return 1;
+//   } else {
+//     // n *n -1
+
+//     return n * factorial(n - 1);
+//   }
+// }
+// console.log(factorial(5));
 
 // "use strict";
 
