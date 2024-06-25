083312d029a124ab4bb85300e388eb48a346b688
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-06-25 07:40:44 +0430

    day 18

---
+++ day 18/script.js
@@ -0,0 +1,11 @@
+function hideimg() {
+  const images = document.getElementsByClassName("contianer");
+
+  images[0].style.display = "none";
+  images[1].style.display = "none";
+}
+
+// function show() {
+//   document.getElementById("img1").style.display = "block";
+//   document.getElementById("img2").style.display = "block";
+// }
